4f4c5b238a9e84f37cd8d1cf5a295b1c4445a9df
K Tran <dangkhoaow@gmail.com>
2026-07-15 05:18:05 +0700

    hub-grid: /space-3d.html sub-tool list -> icon-card grid (33 mini-pictograms)

--- scripts/sitemap-html-builder.mjs
+++ scripts/sitemap-html-builder.mjs
@@ -587,6 +587,7 @@ const HUB_GRID_ROLLOUT = new Set([
   '/image-converter-tools.html',
   '/pdf-tools.html',
   '/developer-tools.html',
+  '/space-3d.html',
 ]);
 
 const MINI_PICTOGRAM_DIR = fileURLToPath(new URL('../source/web/src/main/webapp/static/img/illustrations/mini-pictogram/', import.meta.url));
