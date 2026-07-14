a5b55f7f3979b394e00ddec3035664a9a775db8c
K Tran <dangkhoaow@gmail.com>
2026-07-14 23:25:31 +0700

    hub-grid: /image-converter-tools.html sub-tool list -> icon-card grid (14 mini-pictograms)

--- scripts/sitemap-html-builder.mjs
+++ scripts/sitemap-html-builder.mjs
@@ -584,6 +584,7 @@ const HUB_GRID_ROLLOUT = new Set([
   '/device-test-tools.html',
   '/video-tools.html',
   '/image-tools.html',
+  '/image-converter-tools.html',
 ]);
 
 const MINI_PICTOGRAM_DIR = fileURLToPath(new URL('../source/web/src/main/webapp/static/img/illustrations/mini-pictogram/', import.meta.url));
