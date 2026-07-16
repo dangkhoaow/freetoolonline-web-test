6ab6fb6d6704aa2d27b3ee1e88895560edaf4c93
K Tran <dangkhoaow@gmail.com>
2026-07-16 20:50:59 +0700

    space3d-ship: Solar Analemma 3D Explorer + 18 multilingual guides (fire47)

--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -451,6 +451,7 @@ try {
     { title: "Sunspot Activity 3D Explorer", url: "https://freetoolonline.com/space-3d/sunspot-cycle.html", include: !1, tags: "space-3d" },
     { title: "Uranus Tilt 3D Explorer", url: "https://freetoolonline.com/space-3d/uranus-tilt.html", include: !1, tags: "space-3d" },
     { title: "Roche Limit 3D Explorer", url: "https://freetoolonline.com/space-3d/roche-limit.html", include: !1, tags: "space-3d" },
+    { title: "Solar Analemma 3D Explorer", url: "https://freetoolonline.com/space-3d/solar-analemma.html", include: !1, tags: "space-3d" },
     { title: "Solar Eclipse 3D Explorer", url: "https://freetoolonline.com/space-3d/solar-eclipse.html", include: !1, tags: "space-3d" },
     { title: "Linux Online - Run Linux in Your Browser (Terminal & Desktop)", url: "https://freetoolonline.com/utility-tools/linux-online.html", include: !1, tags: "utility" },
     { title: "Retro FPS Online - Play a Classic Shooter in Your Browser (Freedoom)", url: "https://freetoolonline.com/games/retro-fps-online.html", include: !1, tags: "games" },
