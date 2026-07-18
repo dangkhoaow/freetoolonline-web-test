dcbb360fa060914a934a15a5c10fc64ffa8d2d6c
K Tran <dangkhoaow@gmail.com>
2026-07-18 22:30:00 +0700

    dino3d-ship: Sauropelta viewer + registry wiring (fire106)

--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -389,6 +389,7 @@ try {
     { title: "Tarbosaurus 3D Viewer", url: "https://freetoolonline.com/dinosaur-3d/tarbosaurus.html", include: !1, tags: "dinosaur-3d" },
     { title: "Titanosaurus 3D Viewer", url: "https://freetoolonline.com/dinosaur-3d/titanosaurus.html", include: !1, tags: "dinosaur-3d" },
     { title: "Dracovenator 3D Viewer", url: "https://freetoolonline.com/dinosaur-3d/dracovenator.html", include: !1, tags: "dinosaur-3d" },
+    { title: "Sauropelta 3D Viewer", url: "https://freetoolonline.com/dinosaur-3d/sauropelta.html", include: !1, tags: "dinosaur-3d" },
     { title: "AI Background Remover", url: "https://freetoolonline.com/image-tools/background-remover.html", include: !1, tags: "image-editing" },
     { title: "PDF to Text Online: Extract Text Free, No Install", url: "https://freetoolonline.com/guides/pdf-to-text-online-i-love-pdf.html", include: !1, tags: "pdf" },
     { title: "Video to GIF Converter", url: "https://freetoolonline.com/video-tools/video-to-gif.html", include: !1, tags: "video" },
