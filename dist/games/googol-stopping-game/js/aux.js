c87f1249511d7cb2e406c4046318d7ba0290f246
K Tran <dangkhoaow@gmail.com>
2026-07-21 13:50:28 +0700

    add

--- dist/games/googol-stopping-game/js/aux.js
+++
@@ -1,1 +0,0 @@
-/* ftol adapt: stripped serviceWorker register + beforeinstallprompt (iframe-unsafe / external scope). Upstream: victorqribeiro/googol MIT. */
--- dist/games/pixel-realm-rpg/assets/UI/player_equipment_slots/readme.txt
+++
@@ -1,1 +0,0 @@
-Estos serán los iconos que se mostrarán cuando el jugador no tiene nada en ese slot.
