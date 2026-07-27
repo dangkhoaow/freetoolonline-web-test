e0d3f2ca5e02b444fa74689eed540255acb0f81e
K Tran <dangkhoaow@gmail.com>
2026-07-27 17:10:02 +0700

    cleanup: remove dist/ from git tracking (committed-dist rule)

--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -735,6 +735,7 @@ try {
     { title: "Caspian Sea 3D Explorer", url: "https://freetoolonline.com/places-3d/caspian-sea.html", include: !1, tags: "places-3d" },
     { title: "Lake Baikal 3D Explorer", url: "https://freetoolonline.com/places-3d/lake-baikal.html", include: !1, tags: "places-3d" },
     { title: "Mariana Trench 3D Explorer", url: "https://freetoolonline.com/places-3d/mariana-trench.html", include: !1, tags: "places-3d" },
+    { title: "Black Sea 3D Explorer", url: "https://freetoolonline.com/places-3d/black-sea.html", include: !1, tags: "places-3d" },
     { title: "Algol Eclipsing Binary 3D Explorer", url: "https://freetoolonline.com/space-3d/algol-eclipsing-binary.html", include: !1, tags: "space-3d" },
     { title: "Main-Sequence Lifetime 3D Explorer", url: "https://freetoolonline.com/space-3d/main-sequence-lifetime.html", include: !1, tags: "space-3d" },
     { title: "Earth Perihelion & Aphelion 3D Explorer", url: "https://freetoolonline.com/space-3d/earth-perihelion-aphelion.html", include: !1, tags: "space-3d" },
