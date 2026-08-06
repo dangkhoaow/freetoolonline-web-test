191d41129e60355e4165204c8bb757cd6b983062
K Tran <dangkhoaow@gmail.com>
2026-08-06 18:45:20 +0700

    places-3d: add Uluru 3D explorer (single sandstone monolith archetype)
    Signature figure: an elongated, near-vertical-grain sandstone monolith
    rising abruptly from a flat desert plain (a new terrain archetype for the
    cluster - a single rising body, not a bowl/pit/pillar-field/gorge). Two
    toggles: sunset-glow color shift and a Kata Tjuta marker (~25 km west).
    Published figures: height ~348 m, base circumference ~9.4 km, Arkose
    sandstone ~550 Myr old. Full 12-file bundle: JSP + 7 CMS fragments +
    mini-pictogram SVG + 4 registry-line edits (site-data/seo-clusters/
    related-tools/header-pictogram-enrolled).

---
+++ source/web/src/main/webapp/WEB-INF/jsp/place/uluru.jsp
@@ -0,0 +1,13 @@
+<%@ taglib tagdir='/WEB-INF/tags/' prefix='freetoolonline' %>
+<%@ taglib uri='http://java.sun.com/jsp/jstl/functions' prefix='fn' %>
+<freetoolonline:page
+	customStyle='${pageStyle}'
+	browserTitle='${pageBodyTitle}'
+	keyword='${pageBodyKeyword}'
+	description='${pageBodyDesc}'>
+	<freetoolonline:loading/>
+	${pageBodyHTML}
+	<freetoolonline:welcome welcomeTest='${pageBodyWelcome}'/>
+	<freetoolonline:share-btns></freetoolonline:share-btns>
+	${pageBodyJS}
+</freetoolonline:page>
--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -743,6 +743,7 @@ try {
     { title: "Mount Everest 3D Explorer", url: "https://freetoolonline.com/places-3d/mount-everest.html", include: !1, tags: "places-3d" },
     { title: "Grand Canyon 3D Explorer", url: "https://freetoolonline.com/places-3d/grand-canyon.html", include: !1, tags: "places-3d" },
     { title: "Bryce Canyon 3D Explorer", url: "https://freetoolonline.com/places-3d/bryce-canyon.html", include: !1, tags: "places-3d" },
+    { title: "Uluru 3D Explorer", url: "https://freetoolonline.com/places-3d/uluru.html", include: !1, tags: "places-3d" },
     { title: "Caspian Sea 3D Explorer", url: "https://freetoolonline.com/places-3d/caspian-sea.html", include: !1, tags: "places-3d" },
     { title: "Lake Baikal 3D Explorer", url: "https://freetoolonline.com/places-3d/lake-baikal.html", include: !1, tags: "places-3d" },
     { title: "Milford Sound 3D Explorer", url: "https://freetoolonline.com/places-3d/milford-sound.html", include: !1, tags: "places-3d" },
