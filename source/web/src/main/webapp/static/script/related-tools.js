92057c0fd8247a48a267108bdd7e966fe823c511
K Tran <dangkhoaow@gmail.com>
2026-08-27 16:15:24 +0700

    places-3d: ship Great Rift Valley 3D Explorer (flat trench floor between two escarpment walls, volcanic peaks and rift lakes)

--- source/web/src/main/webapp/static/script/related-tools.js
+++ source/web/src/main/webapp/static/script/related-tools.js
@@ -755,6 +755,7 @@ try {
     { title: "Mekong Delta 3D Explorer", url: "https://freetoolonline.com/places-3d/mekong-delta.html", include: !1, tags: "places-3d" },
     { title: "Kilauea 3D Explorer", url: "https://freetoolonline.com/places-3d/kilauea.html", include: !1, tags: "places-3d" },
     { title: "Victoria Falls 3D Explorer", url: "https://freetoolonline.com/places-3d/victoria-falls.html", include: !1, tags: "places-3d" },
+    { title: "Great Rift Valley 3D Explorer", url: "https://freetoolonline.com/places-3d/great-rift-valley.html", include: !1, tags: "places-3d" },
     { title: "Caspian Sea 3D Explorer", url: "https://freetoolonline.com/places-3d/caspian-sea.html", include: !1, tags: "places-3d" },
     { title: "Lake Baikal 3D Explorer", url: "https://freetoolonline.com/places-3d/lake-baikal.html", include: !1, tags: "places-3d" },
     { title: "Milford Sound 3D Explorer", url: "https://freetoolonline.com/places-3d/milford-sound.html", include: !1, tags: "places-3d" },
